1bf11ba56916967de453cb42983247a8a2ccc4bf
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-06-23 21:51:31 -0500

    have class and component working on table

---
+++ src/app/orbit-list/orbit-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-list',
+  templateUrl: './orbit-list.component.html',
+  styleUrls: ['./orbit-list.component.css']
+})
+export class OrbitListComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() satellites: Satellite[];
+
+  ngOnInit() {
+  }
+
+}
