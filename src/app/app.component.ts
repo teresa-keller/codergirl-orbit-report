5a220a2d1f69e61553b40d3445983d917167f599
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-02 14:09:49 -0500

    search is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,5 @@ export class AppComponent {
     // assign this.displayList to be the array of matching satellites
     // this will cause Angular to re-make the table, but now only containing matches
     this.displayList = matchingSatellites;
- }
-  
+ }  
 }
